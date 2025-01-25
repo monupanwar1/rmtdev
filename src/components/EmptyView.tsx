bfb173188b76182f87cae653ba15e94baf096d27
panwarkunal <monupanwar5655@gmail.com>
2025-01-25 17:19:10 +0530

    empty View

---
+++ src/components/EmptyView.tsx
@@ -0,0 +1,9 @@
+export default function EmptyView() {
+  return (
+    <div>
+        <section className="empty-state"></section>
+        <h3>Empty Packing List</h3>
+        <p>Start by adding items {"don't"} want to forget</p>
+    </div>
+  )
+}
