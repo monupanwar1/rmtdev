a5e30b8f346bc432bdd9445da79c19fe30d1addf
panwarkunal <monupanwar5655@gmail.com>
2025-01-25 17:21:27 +0530

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+        <small> &copy; 2050. Copyright by KunalPanwar</small>
+        <p>
+            Verison <b>2.5</b>
+        </p>
+    </footer>
+  )
+}
