b27813bbfa3b00dc68ec8fb14b9b5928c4d3c3d6
kunal <monupanwar5556@gmail.com>
2025-02-14 09:00:09 +0530

    react hot toast installed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import {useState } from "react";
+import { useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
@@ -6,58 +6,51 @@ import Header, { HeaderTop } from "./Header";
 import Logo from "./Logo";
 import BookmarksButton from "./BookmarksButton";
 import SearchForm from "./SearchForm";
-import Sidebar,{SidebarTop} from "./Sidebar";
+import Sidebar, { SidebarTop } from "./Sidebar";
 import JobItemContent from "./JobItemContent";
 import SortingControls from "./SortingControls";
 import ResultsCount from "./ResultsCount";
 import Pagination from "./PaginationControls";
 import JobList from "./JobList";
 import { useDebounce, useJobItems } from "../lib/hooks";
-
-
-
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const[searchText,setSearchText]=useState("");
-  const debouncedSearchText =useDebounce(searchText,250)
-  const {jobItems,isLoading}=useJobItems(debouncedSearchText);
+  const [searchText, setSearchText] = useState("");
+  const debouncedSearchText = useDebounce(searchText, 250);
+  const { jobItems, isLoading } = useJobItems(debouncedSearchText);
 
   // slicing and returning only 7;
 
-const totalNumberOfResult =jobItems.length;
-const jobItemSliced=jobItems.slice(0,7);
-
-
- 
+  const totalNumberOfResult = jobItems.length;
+  const jobItemSliced = jobItems.slice(0, 7);
 
   return (
     <>
-     <Background/>
-     <Header>
-      <HeaderTop>  
-        <Logo/>
-        <BookmarksButton/>
-      </HeaderTop>
-      
+      <Background />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
 
-        <SearchForm  searchText={searchText}
-        setSearchText={setSearchText}
-        />
-     </Header>
-     <Container>
-     <Sidebar>
-     <SidebarTop>
-            <ResultsCount totalNumberOfResults={totalNumberOfResult}/>
-            <SortingControls/> 
-      </SidebarTop>
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount totalNumberOfResults={totalNumberOfResult} />
+            <SortingControls />
+          </SidebarTop>
 
-        <JobList jobItems={jobItemSliced} isLoading={isLoading}/>
-        <Pagination/>
-      </Sidebar>
+          <JobList jobItems={jobItemSliced} isLoading={isLoading} />
+          <Pagination />
+        </Sidebar>
 
-        <JobItemContent/>
+        <JobItemContent />
       </Container>
-     <Footer/>
+      <Footer />
+      <Toaster position="top-center" reverseOrder={false} />
     </>
   );
 }
